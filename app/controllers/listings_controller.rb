485593e06e298587758b6d15bcbfd7352b991245
Josephine Chong <josephine.yt.chong@gmail.com>
2019-04-17 14:55:29 +1000

    added controllers and methods, plus views from listings

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,29 @@
+class ListingsController < ApplicationController
+    def create
+        #create a new listing
+    end
+    
+    def update
+        #updates current listing
+    end
+
+    def index
+        #shows all listing
+    end
+
+    def edit
+    #show the edit form
+    end
+
+    def destroy
+       #deletes current listing 
+    end
+
+    def new
+    #shows form for creating a new listing
+    end
+
+    def show
+    #view a single listing
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get "/", to: "pages#home", as: "root"
+  resources :listings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
