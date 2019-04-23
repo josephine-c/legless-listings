097cc85074f49a6177dae3e2b7c34609c27e706a
Josephine Chong <josephine.yt.chong@gmail.com>
2019-04-23 15:29:27 +1000

    Aactivated active storage, hooked up to S3, added in devise and setup user and listing associations and added in authorization

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,11 +1,14 @@
 class ListingsController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
     before_action :set_listing, only: [:show, :edit, :update, :destroy]
+    before_action :authorize_user, only: [:edit, :update, :destroy]
     before_action :set_breeds_and_sexes, only: [:new, :edit]
 
     def create
         #create a new listing
         #byebug
-        @listing = Listing.create(listing_params)
+        #@listing = Listing.create(listing_params)
+        @listing = current_user.listings.create(listing_params)
 
         if @listing.errors.any?
             set_breeds_and_sexes
@@ -35,7 +38,6 @@ class ListingsController < ApplicationController
     def new
         #shows form for creating a new listing
         @listing = Listing.new
-        set_breeds_and_sexes
     end
 
     def show
@@ -53,6 +55,19 @@ class ListingsController < ApplicationController
         id = params[:id]
         @listing = Listing.find(id)
     end
+
+    def authorize_user
+        if @listing.user_id != current_user.id
+            redirect_to listings_path
+        end
+    end
+
+    # def set_user_listing
+    #     @listing = Listing.where(user_id: current_user.id, id: params [:id].first)
+    #     if !@listing
+    #         redirect_to listings_path
+    #     end
+    # end
 
     def listing_params
         params.require(:listing).permit(:title, :description, :breed_id, :sex, :price, :deposit, :date_of_birth, :diet, :picture)
